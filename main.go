57a5f1f8b9ac3f8365a7ca909bd1c33b1a56d557
Drew Wells <drew.wells00@gmail.com>
2014-02-14 00:37:16 -0600

    Built run objects from result return

--- main.go
+++ main.go
@@ -11,6 +11,6 @@ var wpturl = "http://webpagetest.eng.wsm.local/"
 func main() {
 	//location := GetLocations(wpturl + "getLocations.php?f=json")
 	//fmt.Printf("%# v", pretty.Formatter(location))
-	results := GetResult(wpturl, "140213_GA_1D8")
-	fmt.Printf("%# v", pretty.Formatter(results))
+	results := GetResult(wpturl, "140214_XW_DS")
+	fmt.Printf("%# v", pretty.Formatter(results.Data.Run[1:]))
 }
--- results.go
+++ results.go
@@ -8,96 +8,145 @@ import (
 )
 
 type WPTResult struct {
+	Run                        int32   `xml:"run"`
 	URL                        string  `xml:"URL"`
 	LoadTime                   int32   `xml:"loadTime"`
 	TTFB                       int32   `xml:"TTFB"`
 	BytesOut                   int32   `xml:"bytesOut"`
 	BytesOutDoc                int32   `xml:"bytesOutDoc"`
-	connections                int32   `xml:"connections"`
-	requests                   int32   `xml:"requests"`
-	requestsDoc                int32   `xml:"requestsDoc"`
-	responses_200              int32   `xml:"responses_200"`
-	responses_404              int32   `xml:"responses_404"`
-	responses_other            int32   `xml:"responses_other"`
-	result                     int32   `xml:"result"`
-	render                     int32   `xml:"render"`
-	fullyLoaded                int32   `xml:"fullyLoaded"`
-	cached                     int32   `xml:"cached"`
-	docTime                    int32   `xml:"docTime"`
-	domTime                    int32   `xml:"domTime"`
-	score_cache                int32   `xml:"score_cache"`
-	score_cdn                  int32   `xml:"score_cdn"`
-	score_gzip                 int32   `xml:"score_gzip"`
-	score_cookies              int32   `xml:"score_cookies"`
-	score_keep_alive           int32   `xml:"score_keep-alive"`
-	score_minify               int32   `xml:"score_minify"`
-	score_combine              int32   `xml:"score_combine"`
-	score_compress             int32   `xml:"score_compress"`
-	score_etags                int32   `xml:"score_etags"`
-	gzip_total                 int32   `xml:"gzip_total"`
-	gzip_savings               int32   `xml:"gzip_savings"`
-	minify_total               int32   `xml:"minify_total"`
-	minify_savings             int32   `xml:"minify_savings"`
-	image_total                int32   `xml:"image_total"`
-	image_savings              int32   `xml:"image_savings"`
-	optimization_checked       int32   `xml:"optimization_checked"`
-	aft                        int32   `xml:"aft"`
-	domElements                int32   `xml:"domElements"`
-	pageSpeedVersion           int32   `xml:"pageSpeedVersion"`
-	title                      int32   `xml:"title"`
-	titleTime                  int32   `xml:"titleTime"`
-	loadEventStart             int32   `xml:"loadEventStart"`
-	loadEventEnd               int32   `xml:"loadEventEnd"`
-	domContentLoadedEventStart int32   `xml:"domContentLoadedEventStart"`
-	domContentLoadedEventEnd   int32   `xml:"domContentLoadedEventEnd"`
-	lastVisualChange           int32   `xml:"lastVisualChange"`
-	browser_name               string  `xml:"browser_name"`
-	browser_version            float32 `xml:"browser_version"`
-	server_count               int32   `xml:"server_count"`
-	server_rtt                 int32   `xml:"server_rtt"`
-	base_page_cdn              string  `xml:"base_page_cdn"`
-	adult_site                 string  `xml:"adult_site"`
-	fixed_viewport             string  `xml:"fixed_viewport"`
-	score_progressive_jpeg     int32   `xml:"score_progressive_jpeg"`
-	firstPaint                 int32   `xml:"firstPaint"`
-	docCPUms                   int32   `xml:"docCPUms"`
-	fullyLoadedCPUms           int32   `xml:"fullyLoadedCPUms"`
-	docCPUpct                  int32   `xml:"docCPUpct"`
-	fullyLoadedCPUpct          int32   `xml:"fullyLoadedCPUpct"`
-	date                       int32   `xml:"date"`
+	Connections                int32   `xml:"connections"`
+	Requests                   int32   `xml:"requests"`
+	RequestsDoc                int32   `xml:"requestsDoc"`
+	Responses_200              int32   `xml:"responses_200"`
+	Responses_404              int32   `xml:"responses_404"`
+	Responses_other            int32   `xml:"responses_other"`
+	Result                     int32   `xml:"result"`
+	Render                     int32   `xml:"render"`
+	FullyLoaded                int32   `xml:"fullyLoaded"`
+	Cached                     int32   `xml:"cached"`
+	DocTime                    int32   `xml:"docTime"`
+	DomTime                    int32   `xml:"domTime"`
+	Score_cache                int32   `xml:"score_cache"`
+	Score_cdn                  int32   `xml:"score_cdn"`
+	Score_gzip                 int32   `xml:"score_gzip"`
+	Score_cookies              int32   `xml:"score_cookies"`
+	Score_keep_alive           int32   `xml:"score_keep-alive"`
+	Score_minify               int32   `xml:"score_minify"`
+	Score_combine              int32   `xml:"score_combine"`
+	Score_compress             int32   `xml:"score_compress"`
+	Score_etags                int32   `xml:"score_etags"`
+	Gzip_total                 int32   `xml:"gzip_total"`
+	Gzip_savings               int32   `xml:"gzip_savings"`
+	Minify_total               int32   `xml:"minify_total"`
+	Minify_savings             int32   `xml:"minify_savings"`
+	Image_total                int32   `xml:"image_total"`
+	Image_savings              int32   `xml:"image_savings"`
+	Optimization_checked       int32   `xml:"optimization_checked"`
+	Aft                        int32   `xml:"aft"`
+	DomElements                int32   `xml:"domElements"`
+	PageSpeedVersion           float32 `xml:"pageSpeedVersion"`
+	Title                      string  `xml:"title"`
+	TitleTime                  int32   `xml:"titleTime"`
+	LoadEventStart             int32   `xml:"loadEventStart"`
+	LoadEventEnd               int32   `xml:"loadEventEnd"`
+	DomContentLoadedEventStart int32   `xml:"domContentLoadedEventStart"`
+	DomContentLoadedEventEnd   int32   `xml:"domContentLoadedEventEnd"`
+	LastVisualChange           int32   `xml:"lastVisualChange"`
+	Browser_name               string  `xml:"browser_name"`
+	Browser_version            string  `xml:"browser_version"`
+	Server_count               int32   `xml:"server_count"`
+	Server_rtt                 int32   `xml:"server_rtt"`
+	Base_page_cdn              string  `xml:"base_page_cdn"`
+	Adult_site                 string  `xml:"adult_site"`
+	Fixed_viewport             string  `xml:"fixed_viewport"`
+	Score_progressive_jpeg     int32   `xml:"score_progressive_jpeg"`
+	FirstPaint                 int32   `xml:"firstPaint"`
+	DocCPUms                   float32 `xml:"docCPUms"`
+	FullyLoadedCPUms           float32 `xml:"fullyLoadedCPUms"`
+	DocCPUpct                  float32 `xml:"docCPUpct"`
+	FullyLoadedCPUpct          float32 `xml:"fullyLoadedCPUpct"`
+	Date                       int32   `xml:"date"`
 	SpeedIndex                 int32   `xml:"SpeedIndex"`
-	visualComplete             int32   `xml:"visualComplete"`
-	effectiveBps               int32   `xml:"effectiveBps"`
-	effectiveBpsDoc            int32   `xml:"effectiveBpsDoc"`
-	avgRun                     int32   `xml:"avgRun"`
+	VisualComplete             int32   `xml:"visualComplete"`
+	//Only available on aggregate runs
+	//EffectiveBps int32 `xml:"effectiveBps"`
+	//EffectiveBpsDoc int32 `xml:"effectiveBpsDoc"`
+	//AvgRun int32 `xml:"avgRun"`
 	//userTime dynamic values
 }
 
-type FirstView struct {
-	FirstView WPTResult `xml:"firstView"`
+type Pages struct {
+	Details    string `xml:"details"`
+	Checklist  string `xml:"checklist"`
+	Breakdown  string `xml:"breakdown"`
+	Domains    string `xml:"domains"`
+	ScreenShot string `xml:"screenShot"`
+}
+
+type Thumbnails struct {
+	WaterFall  string `xml:"waterfall"`
+	Checklist  string `xml:"checklist"`
+	ScreenShot string `xml:"screenShot"`
+}
+
+type Images struct {
+	Thumbnails
+	ConnectionView string `xml:"connectionView"`
+}
+
+type RawData struct {
+	Headers      string `xml:"headers"`
+	PageData     string `xml:"pageData"`
+	RequestsData string `xml:"requestsData"`
+	Utilization  string `xml:"utilization"`
+}
+
+type Frame struct {
+	Time             int32  `xml:"time"`
+	Image            string `xml:"image"`
+	VisuallyComplete int32
+}
+
+type VideoFrames struct {
+	Frame Frame `xml:"frame"`
+}
+
+type WPTResultSet struct {
+	Results     WPTResult   `xml:"results"`
+	Pages       Pages       `xml:"pages"`
+	Thumbnails  Thumbnails  `xml:"thumbnails"`
+	Images      Images      `xml:"images"`
+	RawData     RawData     `xml:"rawData"`
+	VideoFrames VideoFrames `xml:"videoFrames"`
+}
+
+type Views struct {
+	FirstView  WPTResult `xml:"firstView"`
+	RepeatView WPTResult `xml:"repeatView"`
 }
 
 type WPTRun struct {
-	FirstView WPTResult `xml:"firstView>results"`
-	Id        int32     `xml:"id"`
+	FirstView  WPTResultSet `xml:"firstView"`
+	RepeatView WPTResultSet `xml:"repeatView"`
+	Id         int32        `xml:"id"`
 }
 
 type WPTResultData struct {
-	TestId            string    `xml:"testId"`
-	Summary           string    `xml:"summary"`
-	Location          string    `xml:"location"`
-	Connectivity      string    `xml:"connectivity"`
-	BwDown            int32     `xml:"bwDown"`
-	BwUp              int32     `xml:"bwUp"`
-	Latency           int32     `xml:"latency"`
-	Plr               int32     `xml:"plr"`
-	Completed         string    `xml:"completed"`
-	Runs              int32     `xml:"runs"`
-	SuccessfulFVRuns  int32     `xml:"successfulFVRuns"`
-	Average           FirstView `xml:"average"`
-	Median            FirstView `xml:"median"`
-	StandardDeviation FirstView `xml:"standardDeviation"`
-	Run               []WPTRun  `xml:"run"`
+	TestId            string   `xml:"testId"`
+	Summary           string   `xml:"summary"`
+	Location          string   `xml:"location"`
+	Connectivity      string   `xml:"connectivity"`
+	BwDown            int32    `xml:"bwDown"`
+	BwUp              int32    `xml:"bwUp"`
+	Latency           int32    `xml:"latency"`
+	Plr               int32    `xml:"plr"`
+	Completed         string   `xml:"completed"`
+	Runs              int32    `xml:"runs"`
+	SuccessfulFVRuns  int32    `xml:"successfulFVRuns"`
+	Average           Views    `xml:"average"`
+	Median            Views    `xml:"median"`
+	StandardDeviation Views    `xml:"standardDeviation"`
+	Run               []WPTRun `xml:"run"`
 }
 
 type WPTContainer struct {
